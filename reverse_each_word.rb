5ab96b9bdba3574a684cfedde2eec56f28cfaf2e
Ana Riat <t.riat24@gmail.com>
2019-05-12 10:50:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,13 +4,4 @@ def reverse_each_word_(string)
   original_array.each do |string|
     return_array << string.reverse 
   end
-  return_array.join (" ")
-end
-def reverse_each_word(string)
-  array = string.split (" ")
-  test_array + []
-  array.collect do |string|
-    test_array << string.reverse
-  end
-  test_array.join(" ")
-end
+  
